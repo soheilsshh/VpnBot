9522536df7faa0b19b075a71a150fadd8c1543ef
rapexa <rapexamrs@gmail.com>
2025-02-26 04:52:36 -0800

    handle callback

--- bot.py
+++ bot.py
@@ -152,7 +152,7 @@ class VPNBot:
                 'back_to_main': self.back_to_main,
                 'charge_wallet': self.handle_wallet_charge,
                 'service_info': self.show_service_info,
-                'extend_service' : self.handle_purchase_confirmation,
+                'extend_service_' : self.handle_purchase_confirmation,
                 'admin_sales_report': self.show_sales_report,
                 'admin_users': self.manage_users,
                 'admin_discount_codes': self.manage_discount_codes,
